b5370c0cd0821a160bd47dcfce7fa27e6d3a4f97
DavisVaughan <davis@rstudio.com>
2021-04-17 13:49:19 -0400

    Do not throw exceptions across package boundaries
    This can cause issues, especially on Windows 32-bit, and is generally not safe. Instead, we update an error object if an error is thrown, which the client is required to check.

--- inst/include/zones/zones.h
+++ inst/include/zones/zones.h
@@ -15,30 +15,32 @@ namespace zones {
 static
 inline
 const date::time_zone*
-locate_zone(const std::string& name) {
-  typedef const date::time_zone* fn_t(const std::string&);
+locate_zone(const std::string& name, std::string& error) {
+  typedef const date::time_zone* fn_t(const std::string&, std::string&);
   static fn_t *fn = (fn_t*) R_GetCCallable("zones", "api_locate_zone");
-  return fn(name);
+  return fn(name, error);
 }
 
 static
 inline
 date::local_info
 get_local_info(const date::local_seconds& tp,
-               const date::time_zone* p_time_zone) {
-  typedef date::local_info fn_t(const date::local_seconds&, const date::time_zone*);
+               const date::time_zone* p_time_zone,
+               std::string& error) {
+  typedef date::local_info fn_t(const date::local_seconds&, const date::time_zone*, std::string&);
   static fn_t *fn = (fn_t*) R_GetCCallable("zones", "api_get_local_info");
-  return fn(tp, p_time_zone);
+  return fn(tp, p_time_zone, error);
 }
 
 static
 inline
 date::sys_info
 get_sys_info(const date::sys_seconds& tp,
-             const date::time_zone* p_time_zone) {
-  typedef date::sys_info fn_t(const date::sys_seconds&, const date::time_zone*);
+             const date::time_zone* p_time_zone,
+             std::string& error) {
+  typedef date::sys_info fn_t(const date::sys_seconds&, const date::time_zone*, std::string&);
   static fn_t *fn = (fn_t*) R_GetCCallable("zones", "api_get_sys_info");
-  return fn(tp, p_time_zone);
+  return fn(tp, p_time_zone, error);
 }
 
 } // namespace zones
--- src/api.cpp
+++ src/api.cpp
@@ -1,24 +1,62 @@
 #include <date/tz.h>
 #include <cpp11/R.hpp>
+#include <cpp11/protect.hpp>
 #include <R_ext/Rdynload.h> // For DllInfo on R 3.3
 
 // -----------------------------------------------------------------------------
 
+/*
+ * To avoid throwing C++ exceptions across package boundaries, we require
+ * that all API functions have a `std::string& error` argument. The client
+ * is responsible for checking if this is `.empty()` after calling any API
+ * function. They can then handle the error in any way that they require.
+ */
+
+#define BEGIN_ZONES_CPP \
+  try {
+#define END_ZONES_CPP                     \
+  }                                       \
+  catch (std::exception & e) {            \
+    error = e.what();                     \
+  }                                       \
+  catch (...) {                           \
+    error = "C++ error (unknown cause)."; \
+  }
+
+// -----------------------------------------------------------------------------
+
 const date::time_zone*
-api_locate_zone(const std::string& name) {
+api_locate_zone(const std::string& name, std::string& error) {
+  BEGIN_ZONES_CPP
+
   return date::locate_zone(name);
+
+  END_ZONES_CPP
+  return nullptr;
 }
 
 date::local_info
 api_get_local_info(const date::local_seconds& tp,
-                   const date::time_zone* p_time_zone) {
+                   const date::time_zone* p_time_zone,
+                   std::string& error) {
+  BEGIN_ZONES_CPP
+
   return p_time_zone->get_info(tp);
+
+  END_ZONES_CPP
+  return date::local_info{};
 }
 
 date::sys_info
 api_get_sys_info(const date::sys_seconds& tp,
-                 const date::time_zone* p_time_zone) {
+                 const date::time_zone* p_time_zone,
+                 std::string& error) {
+  BEGIN_ZONES_CPP
+
   return p_time_zone->get_info(tp);
+
+  END_ZONES_CPP
+  return date::sys_info{};
 }
 
 // -----------------------------------------------------------------------------
